9853e8eabe4dcfa0102398225e08f05876747f70
Richard Chan <richardchanme@gmail.com>
2017-07-20 23:57:14 -0700

    adding input form on component

--- js/main.jsx
+++ js/main.jsx
@@ -4,6 +4,13 @@ class App extends React.Component {
        <div>
          <h1>Hello JSX and the World!</h1>
          <p>Writing some React</p>
+         <form>
+		<label>
+			Name:
+			<input type="text" name="name" />
+		</label>
+			<input type="submit" value="Submit" />
+		</form>
        </div>
     );
   }
