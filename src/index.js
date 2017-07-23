02cfafc2239643624fd5475cf1c846f95cd24006
Richard Chan <richardchanme@gmail.com>
2017-07-23 16:56:27 -0700

    created, exported and imported search bar component to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBDGvLcPd4bdU0fMpYfclLDEkOL8_1FaX8';
+
 // Create a new component. This component should produce some HTML
 
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
