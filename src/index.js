af280d20a8de3110578b18cc3d93f5e7a30992cc
Richard Chan <richardchanme@gmail.com>
2017-07-23 19:59:35 -0700

    search-bar responding to youtube api search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBDGvLcPd4bdU0fMpYfclLDEkOL8_1FaX8';
+
+YTSearch({key: API_KEY, term: 'surfboard'}, function(data) {
+	console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 
